57a21e1e2d32facfdd475f7912027549b26c07a2
Tj-d-tech <mydidishopp@gmail.com>
2026-02-18 21:16:08 +0000

    Prepare PWA deployment and secure API

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'cfa-express---convertisseur-ultra-rapide',
+  webDir: 'dist'
+};
+
+export default config;
